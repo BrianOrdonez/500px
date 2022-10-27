900069a45dadc6a8c2ee3a8473a75478229ff103
flakeater <brianordonez6994@gmail.com>
2022-10-26 23:11:40 -0400

    google analytics

--- ROOT/src/components/App.jsx
+++ ROOT/src/components/App.jsx
@@ -1,11 +1,12 @@
-import React, { useRef } from 'react';
-import { CssBaseline } from '@mui/material';
-import { Route, Switch } from 'react-router-dom';
+import React, { useRef } from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
 
-import useStyles from './styles';
-import useAlan from './Alan';
+import useStyles from "./styles";
+import useAlan from "./Alan";
 
-import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from ".";
 
 const App = () => {
   const classes = useStyles();
@@ -26,7 +27,7 @@ const App = () => {
           <Route exact path="/actors/:id">
             <Actors />
           </Route>
-          <Route exact path={['/', '/approved']}>
+          <Route exact path={["/", "/approved"]}>
             <Movies />
           </Route>
           <Route exact path="/profile/:id">
